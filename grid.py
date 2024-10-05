ea0aab6a49bf232934122080e1e58c6b23ef00da
ThomHags611 <thomashagos611@gmail.com>
2024-10-04 20:00:34 -0700

    created grid and changes color based on value in index

---
+++ grid.py
@@ -0,0 +1,40 @@
+import pygame
+
+class Grid:
+    def __init__(self):
+        self.num_rows = 20
+        self.num_col = 10
+        self.cell_size = 30
+        self.grid = [[0 for y in range(self.num_col)] for x in range(self.num_rows)]
+        self.colors = self.get_cell_colors()
+
+    def printGrid(self):
+        for x in range(self.num_rows):
+            for y in range(self.num_col):
+                print(self.grid[x][y], end = " ")
+            print()
+
+    #used to fill colors array
+    def get_cell_colors(self):
+        dark_grey = (26, 31, 40)
+        green = (47,230,23)
+        red = (232,18,18)
+        orange = (226,116,17)
+        yellow = (237,234,4)
+        purple = (166,0,247)
+        cyan = (21,204,209)
+        blue = (13,64,216)
+
+        return [dark_grey, green, red, orange, yellow, purple, cyan, blue]
+    
+    def draw(self, screen):
+        for row in range(self.num_rows):
+            for col in range(self.num_col):
+                cell_value = self.grid[row][col]
+                #creates rectangle size and coordinate on window
+                cell_rect = pygame.Rect(col*self.cell_size + 1, row * self.cell_size + 1, self.cell_size -1, self.cell_size -1)
+                pygame.draw.rect(screen,self.colors[cell_value],cell_rect) 
+
+
+
+         
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame, sys
-
+from grid import Grid
 
 pygame.init()  
 dark_blue = (44, 44, 127)
@@ -9,12 +9,21 @@ pygame.display.set_caption("Tetris")
 
 clock = pygame.time.Clock()
 
+game_grid = Grid()
+game_grid.printGrid()
+
+game_grid.grid[0][0] = 1
+game_grid.grid[3][5] = 4
+game_grid.grid[17][8] = 7
 while True:
     for event in pygame.event.get():
+        #loops until we close the window
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
     screen.fill(dark_blue)
+    game_grid.draw(screen)
     pygame.display.update()
     
     clock.tick(60)
